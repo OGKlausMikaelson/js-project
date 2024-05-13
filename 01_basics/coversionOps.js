bc89a413da3e8489061805c44b49f98a4ff27004
Raunak Srivastava <153118278+OGKlausMikaelson@users.noreply.github.com>
2024-05-13 13:26:03 +0000

    coversion operations

---
+++ 01_basics/coversionOps.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+
+console.log(score)
+let value = Number(score)
+console.log(value)
+
+console.log(typeof NaN)
+console.log(typeof undefined)
+
+console.log(2+8)
+console.log(2-8)
+console.log(2*8)
+console.log(67/8)
+
+console.log("1"+2);
+console.log(2+"1");
+console.log("1"+2+2);
+console.log(1+2+"3");
+
+console.log(+true);
+//console.log(true+)
+console.log(+""); // + does the conversion
+
+
