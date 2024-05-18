47253a4d83879f73b83d5368dd37c8eb972e4d32
Raunak Srivastava <153118278+OGKlausMikaelson@users.noreply.github.com>
2024-05-18 14:34:42 +0000

    notes

---
+++ 01_basics/numbers.js
@@ -0,0 +1,22 @@
+const number1 = 100
+
+const value = new Number(400)
+
+console.log(number1);
+console.log(value);
+
+console.log(number1.toString.length)
+
+const value1 = 23.54636
+console.log(value1.toPrecision(3)); // priority from left
+
+const hundred = 1000000000
+console.log(hundred.toLocaleString('en'));
+
+//Maths
+
+console.log(Math);
+console.log(Math.max(2,34,4534,3436363,54525252));
+console.log(Math.abs(-314));
+console.log(Math.random());
+console.log((Math.random()*10) +1)
