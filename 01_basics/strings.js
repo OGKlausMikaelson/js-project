33f5463f11148756fb847802c9bbb19aa13fecf4
Raunak Srivastava <153118278+OGKlausMikaelson@users.noreply.github.com>
2024-05-18 08:04:32 +0000

    notes

---
+++ 01_basics/strings.js
@@ -0,0 +1,23 @@
+const name = "Raunak"
+const repcount = 2
+
+ // console.log(name + repcount + "Value of repos") dont use this it is outdate
+
+ // use string intrapolation using back ticks
+
+ console.log(`Hello I am ${name} and my repo count is ${repcount}`)
+
+ const gamename = new String("Hulk#IITM")
+
+ console.log(gamename[0])
+ console.log(gamename[1])
+ console.log(gamename.__proto__) // outputs object
+
+ console.log(gamename.length)
+ console.log(gamename.toUpperCase())
+ console.log(gamename.charAt(2))
+ console.log(gamename.indexOf("T"))
+
+ gamename
+
+
