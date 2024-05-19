fe73f4f6114de721c75f0da6ad9346352d451b5c
Raunak Srivastava <153118278+OGKlausMikaelson@users.noreply.github.com>
2024-05-19 09:35:36 +0000

    notes

---
+++ 02_ds/arrays.js
@@ -0,0 +1,16 @@
+// collection of multiple items in a single variable name 
+// js array is resizable
+// js array can store mix data-type 
+// array inside array
+// array element cannot be accessed using arbit string
+// js array makes a shallow copy ie same reference point ie when we change in initial it change s in the copy as well
+const myArr = [1,2,3,4,5,6];
+const myHer = ['Thor','Capt. America'];
+
+const myCit = new Array(1,2,34)
+console.log(myArr[2])
+
+//array methods
+
+myArr.push(7)
+console.log(myArr);
